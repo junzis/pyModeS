8a54f927f6f440219393ba1c9532eb91b881778a
Junzi Sun <junzisun@gmail.com>
2018-04-19 09:45:56 +0200

    fix deprecation warning

--- pyModeS/decoder/ehs.py
+++ pyModeS/decoder/ehs.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import, print_function, division
+import warnings
+warnings.simplefilter('once', DeprecationWarning)
 
 from pyModeS.decoder.bds.bds40 import *
 from pyModeS.decoder.bds.bds50 import *
 from pyModeS.decoder.bds.bds60 import *
+from pyModeS.decoder.bds import infer
 
 def BDS(msg):
-    import warnings
-    from pyModeS.decoder.bds import infer
-    warnings.simplefilter('always', DeprecationWarning)
-    warnings.warn("pms.ehs.BDS() is deprecated, use pms.bds.infer() instead", DeprecationWarning)
+    warnings.warn("pms.ehs.BDS() is deprecated, use pms.bds.infer() instead.", DeprecationWarning)
     return infer(msg)
 
 def icao(msg):
