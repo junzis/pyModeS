9ad535dc93f73cbd4371497c751e595ac388519d
Junzi Sun <junzisun@gmail.com>
2019-09-30 23:50:07 +0200

    update package info

--- setup.py
+++ setup.py
@@ -49,21 +49,16 @@ setup(
         "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries",
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
     ],
     # What does your project relate to?
-    keywords="Mode-S ADS-B EHS decoding",
+    keywords="Mode-S ADS-B EHS ELS Comm-B",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
