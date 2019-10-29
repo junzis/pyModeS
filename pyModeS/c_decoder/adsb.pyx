f70d1f2f1f3d28a8e8e765949b6b793aa29b9693
Junzi Sun <junzisun@gmail.com>
2019-10-29 18:03:23 +0100

    clean up useless stuff

--- pyModeS/c_decoder/adsb.pyx
+++ pyModeS/c_decoder/adsb.pyx
@@ -1,18 +1,3 @@
-# Copyright (C) 2015 Junzi Sun (TU Delft)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # cython: language_level=3
 
 """ADS-B Wrapper.
@@ -169,11 +154,11 @@ def velocity(bytes msg, bint rtn_sources=False):
             as refrence, 'mag_north' for magnetic north as reference),
             rate of climb/descent source ('Baro' for barometer, 'GNSS'
             for GNSS constellation).
-            
+
             In the case of surface messages, None will be put in place
             for vertical rate and its respective sources.
     """
-    
+
     cdef int tc = typecode(msg)
 
     if 5 <= tc <= 8:
